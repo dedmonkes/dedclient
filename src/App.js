104fcfbcbec0cba756159de5e72682a5791119ab
sunmarrugo <emarrugo@sunshinebouquet.com>
2021-12-18 19:02:28 -0500

    ded II

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './ast.png';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-        <img src={logo} />
+      <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="ded-logo">DED MONKES</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <span class="ms-1 d-none d-sm-inline">ANNOUNCEMENTS</span> <span class="material-icons">lock</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <span class="ms-1 d-none d-sm-inline">COMMUNITY</span> <span class="material-icons">lock</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <span class="ms-1 d-none d-sm-inline">DEDABASE</span> <span class="material-icons">lock</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <span class="ms-1 d-none d-sm-inline">MARKETS</span> <span class="material-icons">lock</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0">
+                                <span class="ms-1 d-none d-sm-inline">ROADMAP</span> <span class="material-icons">lock</span>
+                            </a>
+                        </li>
+                        <li>
+                            <img src="./monke.png" alt="monke icon" width="128" height="128" />
+                        </li>
+                    </ul>
+                    <span className="mb-4 btn-phantom px-2 py-2">
+                      
+                    <img className="phantom-logo me-4" src="./icons/phantom.png" alt="boo! very scary" /><span className="me-1">CONNECT WALLET</span> <span class="material-icons">lock</span></span>
+                </div>
+            </div>
+            <div className="col py-3"><br />
+              <div className="pt-10">
+                <img src="./icons/wait.png" alt="You'll wait fucker." /><br />
+                WE ARE CURRENTLY WORKING ON GETTING DERUGGED
+              </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
